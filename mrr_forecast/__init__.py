c467a6c883dae7348f294e90f0d40e24d2ecf666
Darrshan Govender <darrshangovender@gmail.com>
2026-09-23 07:39:45 +0200

    Update mrr_forecast/__init__.py

--- mrr_forecast/__init__.py
+++ mrr_forecast/__init__.py
@@ -1,7 +1,7 @@
 """MRR forecasting with Holt-Winters seasonal smoothing (statsmodels)."""
 
-from mrr_forecast.generator import generate_mrr_series
+from mrr_forecast.evaluation import ForecastMetrics, forecast_metrics
 from mrr_forecast.forecaster import MRRForecaster
-from mrr_forecast.evaluation import forecast_metrics, ForecastMetrics
+from mrr_forecast.generator import generate_mrr_series
 
-__all__ = ["generate_mrr_series", "MRRForecaster", "forecast_metrics", "ForecastMetrics"]
+__all__ = ["ForecastMetrics", "MRRForecaster", "forecast_metrics", "generate_mrr_series"]
